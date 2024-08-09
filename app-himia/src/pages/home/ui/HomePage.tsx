8a740b69e4cb82b64e1811f5702cc3a49f9e3508
Alexander <losev.pavlovo152@gmail.com>
2024-08-09 09:36:20 +0300

    first commit

---
+++ app-himia/src/pages/home/ui/HomePage.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <main>
+      <h1>hello world !!!</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero,
+        obcaecati!
+      </p>
+    </main>
+  );
+}
